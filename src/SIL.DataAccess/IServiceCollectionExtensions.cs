16e4f368758c87b2243f3be2380eb03685b94067
Damien Daspit <damien_daspit@sil.org>
2023-03-22 22:15:38 +0700

    Make IIdGenerator transient

--- src/SIL.DataAccess/IServiceCollectionExtensions.cs
+++ src/SIL.DataAccess/IServiceCollectionExtensions.cs
@@ -7,7 +7,7 @@ public static class IServiceCollectionExtensions
         Action<IMemoryDataAccessConfigurator> configure
     )
     {
-        services.AddSingleton<SIL.DataAccess.IIdGenerator, ObjectIdGenerator>();
+        services.AddTransient<SIL.DataAccess.IIdGenerator, ObjectIdGenerator>();
         services.AddScoped<IDataAccessContext, MemoryDataAccessContext>();
         configure(new MemoryDataAccessConfigurator(services));
         return services;
@@ -32,7 +32,7 @@ public static class IServiceCollectionExtensions
         var mongoUrl = new MongoUrl(connectionString);
         var mongoClient = new MongoClient(mongoUrl);
         var database = mongoClient.GetDatabase(mongoUrl.DatabaseName);
-        services.AddSingleton<SIL.DataAccess.IIdGenerator, ObjectIdGenerator>();
+        services.AddTransient<SIL.DataAccess.IIdGenerator, ObjectIdGenerator>();
         services.AddSingleton<IMongoClient>(mongoClient);
         services.AddSingleton(database);
         services.TryAddScoped<IMongoDataAccessContext, MongoDataAccessContext>();
