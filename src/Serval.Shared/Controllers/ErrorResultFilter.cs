efe43ba9abf2d588333692b32828fd90768199ef
Enkidu93 <lowryec17@gcc.edu>
2023-11-07 13:19:13 -0500

    PR review changes

--- src/Serval.Shared/Controllers/ErrorResultFilter.cs
+++ src/Serval.Shared/Controllers/ErrorResultFilter.cs
@@ -12,16 +12,16 @@ namespace Serval.Shared.Controllers
             _logger = loggerFactory.CreateLogger<ErrorResultFilter>();
         }
 
-        public void OnResultExecuted(ResultExecutedContext context) { }
-
-        public void OnResultExecuting(ResultExecutingContext context)
+        public void OnResultExecuted(ResultExecutedContext context)
         {
-            if ((context.Result is ObjectResult r) && (r.StatusCode >= 400))
+            if (context.HttpContext.Response.StatusCode >= 400)
             {
                 _logger.LogInformation(
-                    $"Client {((Controller)context.Controller).User.Identity?.Name?.ToString()} made request:\n {JsonSerializer.Serialize(((Controller)context.Controller).ControllerContext.RouteData.Values, new JsonSerializerOptions { WriteIndented = true })}.\n Serval responded with code {r.StatusCode}. Trace: {Activity.Current?.Id}"
+                    $"Client {((Controller)context.Controller).User.Identity?.Name?.ToString()} made request:\n {JsonSerializer.Serialize(((Controller)context.Controller).ControllerContext.RouteData.Values, new JsonSerializerOptions { WriteIndented = true })}.\n Serval responded with code {context.HttpContext.Response.StatusCode}. Trace: {Activity.Current?.Id}"
                 );
             }
         }
+
+        public void OnResultExecuting(ResultExecutingContext context) { }
     }
 }
