8e62536f70b96809ec14aaaf880db617281b16d0
Enkidu93 <lowryec17@gcc.edu>
2023-11-09 07:50:38 -0500

    Train on all if unspecified

--- src/Serval.Translation/Services/EngineService.cs
+++ src/Serval.Translation/Services/EngineService.cs
@@ -217,7 +217,7 @@ public class EngineService : EntityServiceBase<Engine>, IEngineService
                             if (trainingCorpus.TextIds is not null)
                                 corpus.TrainOnTextIds.Add(trainingCorpus.TextIds);
                         }
-                        else if (trainOn is not null && trainOn.Count == 0)
+                        else if (trainOn is null)
                         {
                             corpus.TrainOnAll = true;
                         }
