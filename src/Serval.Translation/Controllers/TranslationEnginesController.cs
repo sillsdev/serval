827f754fea0d4ab04d9f8457f4e262b5a4ec9126
John Lambert <john_lambert@sil.org>
2023-12-11 16:59:36 -0500

    Document nmt options in general

--- src/Serval.Translation/Controllers/TranslationEnginesController.cs
+++ src/Serval.Translation/Controllers/TranslationEnginesController.cs
@@ -779,7 +779,7 @@ public class TranslationEnginesController : ServalControllerBase
     /// To filter, use the 3 character code for the book of the Bible in the textID while building. See [here](https://github.com/sillsdev/serval/wiki/Versification-in-Serval) for more information.
     ///
     /// The `"options"` parameter of the build config provides the ability to pass build configuration parameters as a JSON object.
-    /// A typical use case would be to set `"options"` to `{"max_steps":10}` in order to configure the maximum
+    /// A [typical use case](https://github.com/sillsdev/serval/wiki/NMT-Job-Settings) would be to set `"options"` to `{"max_steps":10}` in order to configure the maximum
     /// number of training iterations in order to reduce turnaround time for testing purposes. See [this](https://github.com/sillsdev/serval/wiki/Paratext-Key-Terms-Parsing)
     /// for another use of the `"options"` parameter: excluding Paratext key terms data. (By default, Paratext key terms data will be incorporated and used for training).
     /// </remarks>
