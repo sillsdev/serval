f21ad98eea5f3df73b9597fe3a5298a16fed59af
John Lambert <john_lambert@sil.org>
2023-12-13 10:40:15 -0500

    Reviewer comments

--- src/Serval.ApiServer/Contracts/HealthReportEntryDto.cs
+++ src/Serval.ApiServer/Contracts/HealthReportEntryDto.cs
@@ -4,7 +4,7 @@ public class HealthReportEntryDto
 {
     public string Status { get; set; } = default!;
     public string Duration { get; set; } = default!;
-    public string Description { get; set; } = default!;
-    public string Exception { get; set; } = default!;
-    public IDictionary<string, string> Data { get; set; } = default!;
+    public string? Description { get; set; } = default!;
+    public string? Exception { get; set; } = default!;
+    public IDictionary<string, string>? Data { get; set; } = default!;
 }
