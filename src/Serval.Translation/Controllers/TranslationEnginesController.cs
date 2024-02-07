dbe4877c226d78535f3e1ed6a6dfc4a92df48b98
John Lambert <john_lambert@sil.org>
2024-02-07 13:02:48 -0500

    Switch to normal JSON converter

--- src/Serval.Translation/Controllers/TranslationEnginesController.cs
+++ src/Serval.Translation/Controllers/TranslationEnginesController.cs
@@ -1029,9 +1029,9 @@ public class TranslationEnginesController(
         }
         try
         {
-            build.Options = Newtonsoft.Json.JsonConvert.DeserializeObject<IDictionary<string, object>>(
+            build.Options = JsonSerializer.Deserialize<IDictionary<string, object>>(
                 source.Options?.ToString() ?? "{}",
-                new DictionaryJsonConverter()
+                new JsonSerializerOptions { Converters = { new ObjectToInferredTypesConverter() } }
             );
         }
         catch (Exception e)
