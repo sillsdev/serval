2a88ecb2da07736519df5c26bb3aec2ec3567926
John Lambert <john_lambert@sil.org>
2024-02-26 15:29:27 -0500

    fix webhook json (#330)
    make test verify content

--- tests/Serval.Webhooks.Tests/Services/WebhookJobTests.cs
+++ tests/Serval.Webhooks.Tests/Services/WebhookJobTests.cs
@@ -34,9 +34,11 @@ public class WebhookJobTests
         env.MockHttp.Expect("https://test.client.com/hook")
             .WithHeaders(
                 "X-Hub-Signature-256",
-                "sha256=8EC2360A34811845884D8FCE03866EA8FAD9429AAA9E6247D7A817AD2E170B8F"
+                "sha256=B9FE9CD72B68B88D8C3B644ACD5E2A7A5D3DF1A4E5553C13605C67E47D89AB8B"
             )
+            .WithContent("""{"event":"TranslationBuildStarted","payload":{"buildId":"build1","engineId":"engine1"}}""")
             .Respond(HttpStatusCode.OK);
+        // {"Event":"TranslationBuildStarted","Payload":{"BuildId":"build1","EngineId":"engine1"}}
 
         var payload = new { BuildId = "build1", EngineId = "engine1" };
         await env.Job.RunAsync(WebhookEvent.TranslationBuildStarted, "client", payload, CancellationToken.None);
@@ -83,7 +85,7 @@ public class WebhookJobTests
         env.MockHttp.Expect("https://test.client.com/hook")
             .WithHeaders(
                 "X-Hub-Signature-256",
-                "sha256=8EC2360A34811845884D8FCE03866EA8FAD9429AAA9E6247D7A817AD2E170B8F"
+                "sha256=B9FE9CD72B68B88D8C3B644ACD5E2A7A5D3DF1A4E5553C13605C67E47D89AB8B"
             )
             .Respond(HttpStatusCode.RequestTimeout);
 
@@ -112,7 +114,7 @@ public class WebhookJobTests
         env.MockHttp.Expect("https://test.client.com/hook")
             .WithHeaders(
                 "X-Hub-Signature-256",
-                "sha256=8EC2360A34811845884D8FCE03866EA8FAD9429AAA9E6247D7A817AD2E170B8F"
+                "sha256=B9FE9CD72B68B88D8C3B644ACD5E2A7A5D3DF1A4E5553C13605C67E47D89AB8B"
             )
             .Throw(new HttpRequestException());
 
