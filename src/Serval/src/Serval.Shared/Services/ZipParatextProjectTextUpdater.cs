54b9df46c6c54250035ee21112c8fd294135c8f8
Enkidu93 <lowryec17@gcc.edu>
2024-08-01 22:26:25 -0400

    Switch to new Machine classes for usfm updating

---
+++ src/Serval/src/Serval.Shared/Services/ZipParatextProjectTextUpdater.cs
@@ -0,0 +1,28 @@
+namespace Serval.Shared.Services;
+
+public class ZipParatextProjectTextUpdater : ParatextProjectTextUpdaterBase
+{
+    public ZipParatextProjectTextUpdater(IZipContainer container)
+        : base(new ZipParatextProjectSettingsParser(container))
+    {
+        _projectContainer = container;
+    }
+
+    public ZipParatextProjectTextUpdater(IZipContainer container, ParatextProjectSettings settings)
+        : base(settings)
+    {
+        _projectContainer = container;
+    }
+
+    private readonly IZipContainer _projectContainer;
+
+    protected override bool Exists(string fileName)
+    {
+        return _projectContainer.EntryExists(fileName);
+    }
+
+    protected override Stream Open(string fileName)
+    {
+        return _projectContainer.OpenEntry(fileName);
+    }
+}
