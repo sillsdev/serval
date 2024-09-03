b91a235dab7c90c3a1a8a805035ca93e84b0dcc2
John Lambert <john_lambert@sil.org>
2024-09-03 14:57:27 -0400

    Revert - should have been on separate branch

--- src/Machine/src/Serval.Machine.Shared/Services/DistributedReaderWriterLock.cs
+++ src/Machine/src/Serval.Machine.Shared/Services/DistributedReaderWriterLock.cs
@@ -1,25 +1,18 @@
 ﻿namespace Serval.Machine.Shared.Services;
 
-public class DistributedReaderWriterLock(
-    string hostId,
-    IRepository<RWLock> locks,
-    IIdGenerator idGenerator,
-    string id,
-    TimeSpan defaultLifetime
-) : IDistributedReaderWriterLock
+public class DistributedReaderWriterLock(string hostId, IRepository<RWLock> locks, IIdGenerator idGenerator, string id)
+    : IDistributedReaderWriterLock
 {
     private readonly string _hostId = hostId;
     private readonly IRepository<RWLock> _locks = locks;
     private readonly IIdGenerator _idGenerator = idGenerator;
     private readonly string _id = id;
-    private readonly TimeSpan _defaultLifetime = defaultLifetime;
 
     public async Task<IAsyncDisposable> ReaderLockAsync(
         TimeSpan? lifetime = default,
         CancellationToken cancellationToken = default
     )
     {
-        lifetime ??= _defaultLifetime;
         string lockId = _idGenerator.GenerateId();
         if (!await TryAcquireReaderLock(lockId, lifetime, cancellationToken))
         {
@@ -49,7 +42,6 @@ public class DistributedReaderWriterLock(
         CancellationToken cancellationToken = default
     )
     {
-        lifetime ??= _defaultLifetime;
         string lockId = _idGenerator.GenerateId();
         if (!await TryAcquireWriterLock(lockId, lifetime, cancellationToken))
         {
