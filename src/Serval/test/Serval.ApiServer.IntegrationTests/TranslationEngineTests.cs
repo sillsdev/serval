678f03a645182f9cd2dce5a35ef33767211a9b39
Mudiaga Obriki <mudiaga_obriki@sil.org>
2024-10-24 12:16:10 +0100

    Made edits to the PR

--- src/Serval/test/Serval.ApiServer.IntegrationTests/TranslationEngineTests.cs
+++ src/Serval/test/Serval.ApiServer.IntegrationTests/TranslationEngineTests.cs
@@ -1997,7 +1997,8 @@ public class TranslationEngineTests
     {
         private readonly IServiceScope _scope;
         private readonly MongoClient _mongoClient;
-        public readonly IOptionsMonitor<ApiOptions> ApiOptions;
+
+        // public readonly IOptionsMonitor<ApiOptions> ApiOptions;
 
         public TestEnvironment()
         {
@@ -2008,7 +2009,7 @@ public class TranslationEngineTests
             Factory = new ServalWebApplicationFactory();
             _scope = Factory.Services.CreateScope();
             Engines = _scope.ServiceProvider.GetRequiredService<IRepository<Engine>>();
-            ApiOptions = _scope.ServiceProvider.GetRequiredService<IOptionsMonitor<ApiOptions>>();
+            // ApiOptions = _scope.ServiceProvider.GetRequiredService<IOptionsMonitor<ApiOptions>>();
             DataFiles = _scope.ServiceProvider.GetRequiredService<IRepository<DataFiles.Models.DataFile>>();
             Corpora = _scope.ServiceProvider.GetRequiredService<IRepository<DataFiles.Models.Corpus>>();
             Pretranslations = _scope.ServiceProvider.GetRequiredService<
