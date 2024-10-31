4d4b0135017a9cd860d7ab1f60f78b8fa670ef91
John Lambert <john_lambert@sil.org>
2024-10-31 17:09:38 -0400

    Webhook retry (#532)
    * fix up webhook retry
    
    * remove Polly.

--- src/Serval/src/Serval.Webhooks/Configuration/IServalBuilderExtensions.cs
+++ src/Serval/src/Serval.Webhooks/Configuration/IServalBuilderExtensions.cs
@@ -4,14 +4,7 @@ public static class IServalBuilderExtensions
 {
     public static IServalBuilder AddWebhooks(this IServalBuilder builder)
     {
-        builder
-            .Services.AddHttpClient<WebhookJob>()
-            .AddTransientHttpErrorPolicy(b =>
-                b.WaitAndRetryAsync(
-                    7,
-                    retryAttempt => TimeSpan.FromSeconds(2 * retryAttempt) // total 56, less than the 1 minute limit
-                )
-            );
+        builder.Services.AddHttpClient<WebhookJob>();
         builder.Services.AddScoped<IWebhookService, WebhookService>();
         return builder;
     }
