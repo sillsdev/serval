81e2b0bad9b42bebf180d3aaf1b380c19c898e5f
Enkidu93 <lowryec17@gcc.edu>
2024-12-16 16:31:25 -0500

    Passing API tests
    Fix more names

--- src/Serval/src/Serval.Translation/Services/EngineService.cs
+++ src/Serval/src/Serval.Translation/Services/EngineService.cs
@@ -143,13 +143,9 @@ public class EngineService(
         {
             engine.DateCreated = DateTime.UtcNow;
             await Entities.InsertAsync(engine, cancellationToken);
-            TranslationEngineApi.TranslationEngineApiClient? client;
-            try
-            {
-                client = _grpcClientFactory.CreateClient<TranslationEngineApi.TranslationEngineApiClient>(engine.Type);
-            }
-            catch (InvalidOperationException)
-            {
+            TranslationEngineApi.TranslationEngineApiClient? client =
+                _grpcClientFactory.CreateClient<TranslationEngineApi.TranslationEngineApiClient>(engine.Type);
+            if (client is null)
                 throw new InvalidOperationException($"'{engine.Type}' is an invalid engine type.");
 
             var request = new CreateRequest
