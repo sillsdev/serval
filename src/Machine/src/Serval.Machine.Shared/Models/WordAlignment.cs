af99ef68d4faa75a71f651c753b0fa71abc6dce2
Enkidu93 <lowryec17@gcc.edu>
2025-02-03 11:01:16 -0500

    More small rebase-related changes; remove unnecessary files

--- src/Machine/src/Serval.Machine.Shared/Models/WordAlignment.cs
+++ src/Machine/src/Serval.Machine.Shared/Models/WordAlignment.cs
@@ -7,6 +7,6 @@ public record WordAlignment
     public required IReadOnlyList<string> Refs { get; init; }
     public required IReadOnlyList<string> SourceTokens { get; set; }
     public required IReadOnlyList<string> TargetTokens { get; set; }
-    public required IReadOnlyList<double> Confidences { get; set; } //TODO It seems to me that it'd more natural to have the confidence as part of the word pair object - but I understand that this is currently not the case with the translation result; would it be breaking to change it there too?
+    public required IReadOnlyList<double> Confidences { get; set; }
     public required IReadOnlyList<AlignedWordPair> Alignment { get; set; }
 }
