5a53e666d0ed5ab2077b36cd6693a15ed0de9944
Enkidu93 <lowryec17@gcc.edu>
2025-10-10 17:45:12 -0400

    Make it optional again

--- src/Serval/src/Serval.Translation/Contracts/ParallelCorpusAnalysisDto.cs
+++ src/Serval/src/Serval.Translation/Contracts/ParallelCorpusAnalysisDto.cs
@@ -3,10 +3,9 @@
 public record ParallelCorpusAnalysisDto
 {
     public required string ParallelCorpusRef { get; init; }
-#pragma warning disable CS9042
+
     [Obsolete]
-    public required string SourceQuoteConvention { get; init; }
-#pragma warning restore CS9042
+    public string? SourceQuoteConvention { get; init; }
     public required string TargetQuoteConvention { get; init; }
     public bool CanDenormalizeQuotes { get; init; }
 }
