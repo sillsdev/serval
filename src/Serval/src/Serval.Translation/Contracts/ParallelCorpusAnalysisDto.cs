ee99117a5ac428d1d4eb2433358cb0e387908f10
Enkidu93 <lowryec17@gcc.edu>
2025-10-10 17:37:49 -0400

    Ignore obsolete

--- src/Serval/src/Serval.Translation/Contracts/ParallelCorpusAnalysisDto.cs
+++ src/Serval/src/Serval.Translation/Contracts/ParallelCorpusAnalysisDto.cs
@@ -3,12 +3,10 @@
 public record ParallelCorpusAnalysisDto
 {
     public required string ParallelCorpusRef { get; init; }
-    public required string SourceQuoteConvention
-    {
-        [Obsolete]
-        get;
-        init;
-    }
+#pragma warning disable CS9042
+    [Obsolete]
+    public required string SourceQuoteConvention { get; init; }
+#pragma warning restore CS9042
     public required string TargetQuoteConvention { get; init; }
     public bool CanDenormalizeQuotes { get; init; }
 }
