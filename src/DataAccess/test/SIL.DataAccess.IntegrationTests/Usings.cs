99191885b11c8152c8e5572957355d1fd0ad6e26
Peter Chapman <peter@conglomo.co.nz>
2026-03-16 12:07:47 +1300

    Add testing for schema versioning and migrations

---
+++ src/DataAccess/test/SIL.DataAccess.IntegrationTests/Usings.cs
@@ -0,0 +1,7 @@
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using MongoDB.Bson;
+global using MongoDB.Driver;
+global using NUnit.Framework;
+global using SIL.DataAccess.Models;
+global using SIL.ObjectModel;
--- src/Serval/src/Serval.Shared/Configuration/ServalBuilder.cs
+++ src/Serval/src/Serval.Shared/Configuration/ServalBuilder.cs
@@ -1,6 +1,6 @@
 ﻿namespace Microsoft.Extensions.DependencyInjection;
 
-internal class ServalBuilder(IServiceCollection services, IConfiguration configuration) : IServalBuilder
+public class ServalBuilder(IServiceCollection services, IConfiguration configuration) : IServalBuilder
 {
     public IServiceCollection Services { get; } = services;
     public IConfiguration Configuration { get; } = configuration;
