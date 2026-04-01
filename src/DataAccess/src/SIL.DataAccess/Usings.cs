19dd6ad55935b201c0feac92cdd814ce046218fa
Damien Daspit <damien_daspit@sil.org>
2026-04-01 09:32:28 -0400

    Create SIL.DataAccess.Abstractions assembly

--- src/DataAccess/src/SIL.DataAccess/Usings.cs
+++ src/DataAccess/src/SIL.DataAccess/Usings.cs
@@ -1,6 +1,5 @@
 ﻿global using System.Collections;
 global using System.Diagnostics;
-global using System.Diagnostics.CodeAnalysis;
 global using System.Linq.Expressions;
 global using System.Reflection;
 global using Microsoft.Extensions.DependencyInjection.Extensions;
