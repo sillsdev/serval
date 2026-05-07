5563110181f2a9ce5e68b7418fefe6cdc437e84a
Enkidu93 <lowryec17@gcc.edu>
2026-05-07 12:35:41 -0400

    Update vulnerable dependency

--- src/Serval/test/Serval.E2ETests/Usings.cs
+++ src/Serval/test/Serval.E2ETests/Usings.cs
@@ -1,5 +1,4 @@
-﻿global using System.Globalization;
-global using System.IO.Compression;
+﻿global using System.IO.Compression;
 global using System.Text.Json;
 global using NUnit.Framework;
 global using Serval.Client;
