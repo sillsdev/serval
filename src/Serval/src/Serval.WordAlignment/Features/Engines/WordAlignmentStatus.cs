decc2583ba33e84ff11c0907cc620e53ec18e168
Enkidu93 <lowryec17@gcc.edu>
2026-06-11 15:45:09 -0400

    Remove unused method

--- src/Serval/src/Serval.WordAlignment/Features/Engines/WordAlignmentStatus.cs
+++ src/Serval/src/Serval.WordAlignment/Features/Engines/WordAlignmentStatus.cs
@@ -9,15 +9,6 @@ public enum WordAlignmentStatus
 
 public static class EngineExtensions
 {
-    public static WordAlignmentStatus GetCorpusWordAlignmentStatus(this Engine engine, string corpusId)
-    {
-        if (!engine.ParallelCorpora.Any(c => c.Id == corpusId))
-            return WordAlignmentStatus.CorpusNotFound;
-        if (engine.ModelRevision == 0)
-            return WordAlignmentStatus.NotBuilt;
-        return WordAlignmentStatus.Found;
-    }
-
     public static WordAlignmentStatus GetParallelCorpusWordAlignmentStatus(this Engine engine, string parallelCorpusId)
     {
         if (!engine.ParallelCorpora.Any(c => c.Id == parallelCorpusId))
