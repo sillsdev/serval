8869ceadfd97ca549a9562fa6a99e599f8127445
Enkidu93 <lowryec17@gcc.edu>
2026-07-08 15:43:06 -0400

    Use read-only dictionary where possible

--- src/Machine/src/Serval.Machine.Shared/Services/TranslationPreprocessBuildJob.cs
+++ src/Machine/src/Serval.Machine.Shared/Services/TranslationPreprocessBuildJob.cs
@@ -129,8 +129,14 @@ public class TranslationPreprocessBuildJob(
             InferenceCount = stats.InferenceCount,
             IsTrainFilteredByChapter = stats.IsTrainFilteredByChapter,
             IsInferenceFilteredByChapter = stats.IsInferenceFilteredByChapter,
-            TrainVerseCount = stats.TrainVerseCount,
-            InferenceVerseCount = stats.InferenceVerseCount,
+            TrainVerseCount = stats.TrainVerseCount.ToDictionary(
+                kv => kv.Key,
+                kv => (IReadOnlyDictionary<string, int>)kv.Value
+            ),
+            InferenceVerseCount = stats.InferenceVerseCount.ToDictionary(
+                kv => kv.Key,
+                kv => (IReadOnlyDictionary<string, int>)kv.Value
+            ),
             Warnings = warnings,
             EngineSourceLanguageTag = sourceLanguageTag,
             EngineTargetLanguageTag = targetLanguageTag,
